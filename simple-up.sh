be9e0f6504a0d5f440316e8a94dbc59601cd91e7
Chuck Sanders <nathhad@gmail.com>
2021-11-14 21:06:26 -0500

    Update simple-up.sh for image name

--- simple-up.sh
+++ simple-up.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 docker run -dit \
---name r19dev \
+--name r19build \
 --privileged \
 --log-driver syslog \
 --mount src=r19_autobuild,dst=/build/autobuild \
 --mount src=r19_build,dst=/build/rooter \
 --mount src=r19_output,dst=/build/output \
 --mount type=tmpfs,dst=/tmp/ \
-rooter19076:11.1.02.d
+rooter19076:latest
